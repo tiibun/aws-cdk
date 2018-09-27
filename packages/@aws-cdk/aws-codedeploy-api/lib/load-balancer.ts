5f4326872a397b0ab9cb2a299b0bd1b80c44b6e9
Elad Ben-Israel <benisrae@amazon.com>
2018-09-27 20:24:49 +0300

    Fix indentation of new code

--- packages/@aws-cdk/aws-codedeploy-api/lib/load-balancer.ts
+++ packages/@aws-cdk/aws-codedeploy-api/lib/load-balancer.ts
@@ -2,28 +2,28 @@
  * The generations of AWS load balancing solutions.
  */
 export enum LoadBalancerGeneration {
-    /**
-     * The first generation (ELB Classic).
-     */
-    FIRST,
+  /**
+   * The first generation (ELB Classic).
+   */
+  FIRST,
 
-    /**
-     * The second generation (ALB and NLB).
-     */
-    SECOND
+  /**
+   * The second generation (ALB and NLB).
+   */
+  SECOND
 }
 
 /**
  * An interface of an abstract laod balancer, as needed by CodeDeploy.
  */
 export interface LoadBalancerProvider {
-    /**
-     * Specify the generation this load balancer belongs to.
-     */
-    codeDeployLoadBalancerGeneration(): LoadBalancerGeneration;
+  /**
+   * Specify the generation this load balancer belongs to.
+   */
+  codeDeployLoadBalancerGeneration(): LoadBalancerGeneration;
 
-    /**
-     * Specify the name of this load balancer.
-     */
-    codeDeployLoadBalancerName(): string;
+  /**
+   * Specify the name of this load balancer.
+   */
+  codeDeployLoadBalancerName(): string;
 }
